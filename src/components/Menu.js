5dbfaa2d3d884598ccd67db9e51ad831a96fde15
Shubham-344 <shubhamanand030404@gmail.com>
2024-01-22 18:20:05 +0530

    images added

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,8 +1,15 @@
 const Menu = () => {
     return (
-      <div className="">Menu</div>
+      <div className="">
+        <div className="fooditem">1</div>
+        <div className="fooditem">2</div>
+        <div className="fooditem">3</div>
+        <div className="fooditem">4</div>
+        <div className="fooditem">5</div>
+        <div className="fooditem">6</div>
+      </div>
     );
   };
-  
+
   export default Menu;
   
