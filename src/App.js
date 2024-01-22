44db30112d7868f272b27bc2b57d7f6a39250203
Shubham-344 <shubhamanand030404@gmail.com>
2024-01-23 01:57:35 +0530

    Menu Order Items

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ import { Route, Routes } from "react-router-dom";
 import Stores from './components/Stores';
 import Footer from './components/Footer';
 import Aboutus from './components/Aboutus';
+import Veg_Pizza_item from './components/Veg_Pizza_item';
+import Non_Veg_Pizza_Item from './components/Non_Veg_Pizza_Item';
+import Slice from './components/Slice';
+import Beverages from './components/Beverages';
+import Snacks from './components/Snack';
+import AddOns from './components/AddOns';
+import Delivery from './components/Delivery';
 
 function App() {
   return (
@@ -18,6 +25,13 @@ function App() {
         <Route path="/aboutus" element={<Aboutus/>} />
         <Route path="/contactus" element={<ContactUs/>} />
         <Route path="/" element={<Home/>} />
+        <Route path="/Veg_Pizza_item" element={<Veg_Pizza_item/>} />
+        <Route path="/Non_Veg_Pizza_item" element={<Non_Veg_Pizza_Item/>} />
+        <Route path="/Slice" element={<Slice/>} />
+        <Route path="/Beverages" element={<Beverages/>} />
+        <Route path="/Snack" element={<Snacks/>} />
+        <Route path="/Addon" element={<AddOns/>} />
+        <Route path="/delivery" element={<Delivery/>} />
       </Routes>
       <Footer/>
     </div>
